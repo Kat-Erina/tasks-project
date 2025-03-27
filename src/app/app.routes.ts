7f9aa7c6fe10549d98b61fa9d1e7199a0eca99b5
Kat-Erina <nalchevanidzekaterina@gmail.com>
2025-03-27 18:48:46 +0400

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", 
+        loadComponent:()=>import('./tasks/tasks.component').then(m=>m.TasksComponent)
+    },
+    {
+path:'new-task', 
+loadComponent:()=>import('./new-task/new-task.component').then(m=>m.NewTaskComponent)
+    }
+];
